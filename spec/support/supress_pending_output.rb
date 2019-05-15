4946b27611590e5d167df3c16a2decb8724a43e4
kvokka <kvokka@yahoo.com>
2019-05-15 01:40:22 +0100

    Supress extra output from pending tests from RSpec

---
+++ spec/support/supress_pending_output.rb
@@ -0,0 +1,11 @@
+# Remove output from pending tests
+
+module FormatterOverrides
+  def example_pending(*)
+  end
+
+  def dump_pending(*)
+  end
+end
+
+RSpec::Core::Formatters::ProgressFormatter.prepend FormatterOverrides
